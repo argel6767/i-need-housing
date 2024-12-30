5e72bd9006333b174ed5e04fe979cdbd61edc8ed
argel6767 <argel6767@gmail.com>
2024-12-29 22:53:08 -0500

    begin RentCastApiService

--- backend/src/main/java/com/ineedhousing/backend/apis/RentCastAPIService.java
+++ backend/src/main/java/com/ineedhousing/backend/apis/RentCastAPIService.java
@@ -1,8 +1,15 @@
 package com.ineedhousing.backend.apis;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
-public class RentCastAPIService {
+public class RentCastApiService {
+    private final RestClient restClient;
 
+    public RentCastApiService (@Qualifier("RentCast API") RestClient restClient) {
+        this.restClient = restClient;
+    }
+    
 }
