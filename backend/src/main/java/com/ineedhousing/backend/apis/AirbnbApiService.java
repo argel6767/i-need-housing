c906192838dee74f3d90c46d03e474ff27e0c351
argel6767 <argel6767@gmail.com>
2024-12-30 21:37:24 -0500

    add updateHousingListingsViaCoordinates

--- backend/src/main/java/com/ineedhousing/backend/apis/AirbnbApiService.java
+++ backend/src/main/java/com/ineedhousing/backend/apis/AirbnbApiService.java
@@ -11,11 +11,14 @@ import org.locationtech.jts.geom.Point;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriBuilder;
 
 import com.ineedhousing.backend.apis.exceptions.FailedApiCallException;
 import com.ineedhousing.backend.apis.exceptions.NoListingsFoundException;
 import com.ineedhousing.backend.housing_listings.HousingListing;
 import com.ineedhousing.backend.housing_listings.HousingListingRepository;
+
+import jakarta.persistence.criteria.CriteriaBuilder.In;
 
 /**
  * Houses business logic for Airbnb calls
@@ -37,19 +40,19 @@ public class AirbnbApiService {
         this.housingListingRepository = housingListingRepository;
     }
 
+    /**
+     * grabs new Listings from Airbnb and makes new HousingListing Entities in DB
+     * @param city
+     * @param checkIn
+     * @param checkOut
+     * @param numOfPets
+     * @return List<HousingListing>
+     */
     public List<HousingListing> updateListingViaLocation(String city, LocalDate checkIn, LocalDate checkOut, Integer numOfPets) {
         Map response = restClient.get()
-        .uri(uriBuilder -> uriBuilder
-        .path("/search-location")
-        .queryParam("location", city)
-        .queryParam("checkin", checkIn)
-        .queryParam("checkout", checkOut)
-        .queryParam("adults", ADULTS)
-        .queryParam("children", CHILDREN)
-        .queryParam("infants", INFANTS)
-        .queryParam("pets", numOfPets)
-        .queryParam("page", PAGES)
-        .queryParam("currency", CURRENCY)
+        .uri(uriBuilder -> 
+            applyCommonQueryParams(uriBuilder.path("/search-location").queryParam("location", city), // Unique parameter
+            checkIn, checkOut, numOfPets) // Shared parameters
         .build())
         .retrieve()
         .body(Map.class);
@@ -63,6 +66,44 @@ public class AirbnbApiService {
         return housingListingRepository.saveAll(newListings);
     }
 
+    /**
+     * grabs new Listings from Airbnb and makes new HouseListings Entities in DB
+     * @param neLat
+     * @param neLong
+     * @param swLat
+     * @param swLong
+     * @param checkIn
+     * @param checkOut
+     * @param numOfPets
+     * @return List<HousingListing>
+     */
+    public List<HousingListing> updateHousingListingsViaGeoCoordinates(Double neLat, Double neLong, Double swLat, Double swLong, LocalDate checkIn, LocalDate checkOut, Integer numOfPets) {
+        Map response = restClient.get()
+        .uri(uriBuilder -> applyCommonQueryParams(uriBuilder
+        .path("/search-geo")
+        .queryParam("ne_lat", neLat)
+        .queryParam("ne_lng", neLong)
+        .queryParam("sw_lat", swLat)
+        .queryParam("sw_lng", swLong), checkIn, checkOut, 0)
+        .build())
+        .retrieve()
+        .body(Map.class);
+        if (response == null) {
+            throw new FailedApiCallException("Api call failed to occur. Check usage rates, and make sure your inputs are valid");
+        }
+        if (response.isEmpty()) {
+            throw new NoListingsFoundException(String.format("No listings found within coordinates given, between %s - %s", checkIn, checkOut));
+        }
+        List<HousingListing> newListings = createNewListings(numOfPets, response);
+        return housingListingRepository.saveAll(newListings);
+    }
+
+    /**
+     * contains the abstracted logic of mapping response values to new HouseListing Entities
+     * @param numOfPets
+     * @param response
+     * @return
+     */
     private List<HousingListing> createNewListings(Integer numOfPets, Map response) {
         GeometryFactory factory = GeometrySingleton.getInstance();
         List<Map<String, Object>> listings = (List<Map<String, Object>>) response.get("results");
@@ -93,5 +134,25 @@ public class AirbnbApiService {
         });
         return newListings;
     }
+
+    /**
+     * contains the abstracted logic of common query parameters between api calls
+     * @param uriBuilder
+     * @param checkIn
+     * @param checkOut
+     * @param numOfPets
+     * @return uriBuilder
+     */
+    private UriBuilder applyCommonQueryParams(UriBuilder uriBuilder, LocalDate checkIn, LocalDate checkOut, int numOfPets) {
+    return uriBuilder
+            .queryParam("checkin", checkIn)
+            .queryParam("checkout", checkOut)
+            .queryParam("adults", ADULTS)
+            .queryParam("children", CHILDREN)
+            .queryParam("infants", INFANTS)
+            .queryParam("pets", numOfPets)
+            .queryParam("page", PAGES)
+            .queryParam("currency", CURRENCY);
+}
 
 }
---
+++ backend/src/main/java/com/ineedhousing/backend/apis/requests/AirbnbGeoCoordinateRequest.java
@@ -0,0 +1,20 @@
+package com.ineedhousing.backend.apis.requests;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AirbnbGeoCoordinateRequest {
+
+    private List<Double> areaCorners;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+    private Integer numOfPets;
+    
+}
