88933c1f7d948f849c8761f21dd9e220ef115e6e
Argel Hernandez <argel6767@gmail.com>
2025-01-31 01:28:03 -0500

    containerize backend for prod

--- backend/src/main/java/com/ineedhousing/backend/email/EmailService.java
+++ backend/src/main/java/com/ineedhousing/backend/email/EmailService.java
@@ -4,6 +4,7 @@ package com.ineedhousing.backend.email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,9 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${support.email}")
+    private String emailUsername;
+
     /*
      * sending verification email
      * takes in to whom its being sent to & subject and body
@@ -24,6 +28,7 @@ public class EmailService {
     public void sendEmail(String to, String subject, String body) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
+	helper.setFrom(emailUsername);
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(body, true);
