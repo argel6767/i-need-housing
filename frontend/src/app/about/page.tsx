5a2840b8108f7b1281ebd1964449a35176ad6fa9
argel6767 <argel6767@gmail.com>
2025-04-28 00:02:26 -0400

    docs: add license

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -9,8 +9,8 @@ const About = () => {
             <Navbar/>
             <article id="about" className="px-4 space-y-2">
                 <h1 className="text-3xl font-semibold italic">About Us</h1>
-                <p>INeedHousing was conjured and developed by Argel Hernandez Amaya (me) as a platform to ease the difficulty of finding temp housing for students during their internships.
-                    By leveraging multiple third parties services, such as RentCast and Airbnb, INeedHousing can be used as an aggregate, allowing ease of access for users.
+                <p>INeedHousing was conjured and developed by Argel Hernandez Amaya (me) as a platform to ease the difficulty of finding housing, whether it be temporary or permanent.
+                    By leveraging multiple third parties services, such as RentCast, Airbnb, and Zillow, INeedHousing can be used as an aggregate, allowing ease of access for users.
                 </p>
             </article>
             <article id="contact" className="px-4 space-y-2">
@@ -20,7 +20,7 @@ const About = () => {
             </article>
             <article id="contribution" className="px-4 space-y-2">
                 <h1 className="text-3xl font-semibold italic">Contributing</h1>
-                <p>As of right now no outside contribution is needed. However, I am always happy to take suggestions and/or potential imporvements to the site's visuals and functionalities.</p>
+                <p>As of right now no outside contribution is needed. However, I am always happy to take suggestions and/or potential improvements to the site's visuals and functionalities.</p>
             </article>
             <div className="border-t-2">
                <Footer/> 
