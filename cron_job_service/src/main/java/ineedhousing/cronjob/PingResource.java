45b576e3dc4d91a01f48f1f185964a66850275f5
Argel Hernandez <argel6767@gmail.com>
2025-08-09 00:24:06 -0500

    update /ping

--- cron_job_service/src/main/java/ineedhousing/cronjob/PingResource.java
+++ cron_job_service/src/main/java/ineedhousing/cronjob/PingResource.java
@@ -1,13 +1,29 @@
 package ineedhousing.cronjob;
 
+import io.smallrye.health.SmallRyeHealth;
+import io.smallrye.health.api.HealthRegistry;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Any;
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ApplicationScoped
 @Path("")
 public class PingResource {
+
+    @Inject
+    @Any
+    Instance<HealthCheck> healthChecks;
 
     /**
      * Pings service to wake up/check on it
@@ -15,8 +31,14 @@ public class PingResource {
      */
     @GET
     @Path("/ping")
-    public Response pingService() {
-        return Response.ok().build();
+    @Produces(MediaType.APPLICATION_JSON)
+    public HealthPing pingService() {
+        List<HealthCheckResponse> healthCheckResponses = healthChecks.stream()
+                .map(HealthCheck::call)
+                .toList();
+        return new HealthPing("Cron_Job_Service pinged. Health info:", healthCheckResponses);
     }
 
 }
+
+record HealthPing(String message, List<HealthCheckResponse> healthCheckResponses) {}
