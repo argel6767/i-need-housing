7856a82b60586203629df4221636346878eb4816
argel6767 <argel6767@gmail.com>
2025-08-22 01:09:47 -0400

    allow access to health endpoints

--- new_listings_service/src/main/java/com/ineedhousing/new_listings_service/filters/ApiTokenFilter.java
+++ new_listings_service/src/main/java/com/ineedhousing/new_listings_service/filters/ApiTokenFilter.java
@@ -1,18 +1,20 @@
 package com.ineedhousing.new_listings_service.filters;
 
-import com.ineedhousing.new_listings_service.models.ApiTokenAuthenticationToken;
-import com.ineedhousing.new_listings_service.services.ServiceRegistrationService;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+import com.ineedhousing.new_listings_service.models.ApiTokenAuthenticationToken;
+import com.ineedhousing.new_listings_service.services.ServiceRegistrationService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class ApiTokenFilter extends OncePerRequestFilter {
@@ -26,6 +28,14 @@ public class ApiTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+        if (requestURI.startsWith("/actuator")) {
+            logger.debug("Skipping token validation for actuator endpoint: {}", requestURI);
+            filterChain.doFilter(request, response);
+            return;
+        }
+        
         String apiToken = request.getHeader("X-Api-Token");
         String serviceName = request.getHeader("X-Service-Name");
 
