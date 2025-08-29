413345f75c8907dd4d59a0fe2dba11b9b8d7b8e3
argel6767 <argel6767@gmail.com>
2025-08-29 16:04:29 -0400

    add retry and longer timeout time to accoutn for service booting up

--- keymaster_service/src/main/java/com/ineedhousing/rest_clients/MainAPIEmailServiceRestClient.java
+++ keymaster_service/src/main/java/com/ineedhousing/rest_clients/MainAPIEmailServiceRestClient.java
@@ -1,8 +1,8 @@
 package com.ineedhousing.rest_clients;
 
-import com.ineedhousing.models.SuccessfulKeyRotationEvent;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(configKey = "api-email")
@@ -12,5 +12,6 @@ public interface MainAPIEmailServiceRestClient {
     @Path("/emails/notifications/key-rotation")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Retry(maxRetries = 5, delay = 1000)
     String notifyNewKeyRotation(@HeaderParam("X-Api-Token") String apiToken, @HeaderParam("X-Service-Name") String serviceName, String eventJson);
 }
