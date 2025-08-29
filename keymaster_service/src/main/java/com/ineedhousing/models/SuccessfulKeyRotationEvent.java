57c266213af73fc484b400a664f0270ba4d3cc8c
argel6767 <argel6767@gmail.com>
2025-08-28 23:57:48 -0400

    add log for initial registration

--- keymaster_service/src/main/java/com/ineedhousing/models/SuccessfulKeyRotationEvent.java
+++ keymaster_service/src/main/java/com/ineedhousing/models/SuccessfulKeyRotationEvent.java
@@ -1,6 +1,9 @@
 package com.ineedhousing.models;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import java.time.LocalDateTime;
 
+@RegisterForReflection(serialization = true)
 public record SuccessfulKeyRotationEvent(String message, String newKey, LocalDateTime timeStamp) {
 }
