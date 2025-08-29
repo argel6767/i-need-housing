f6cd402931e640bd8699c522dc00ea8bc15d4351
argel6767 <argel6767@gmail.com>
2025-08-29 19:49:23 -0400

    add more @RegisterRefelction

--- keymaster_service/src/main/java/com/ineedhousing/models/RotatingKeyEvent.java
+++ keymaster_service/src/main/java/com/ineedhousing/models/RotatingKeyEvent.java
@@ -1,6 +1,9 @@
 package com.ineedhousing.models;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import java.time.LocalDateTime;
 
+@RegisterForReflection
 public record RotatingKeyEvent(String message, LocalDateTime timeStamp) {
 }
