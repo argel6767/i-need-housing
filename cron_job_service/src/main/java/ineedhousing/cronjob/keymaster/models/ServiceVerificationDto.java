b38007d9a031605a442f6e3fdac25da1df8b3529
argel6767 <argel6767@gmail.com>
2025-08-29 19:34:25 -0400

    register dtos for reflection

--- cron_job_service/src/main/java/ineedhousing/cronjob/keymaster/models/ServiceVerificationDto.java
+++ cron_job_service/src/main/java/ineedhousing/cronjob/keymaster/models/ServiceVerificationDto.java
@@ -1,6 +1,9 @@
 package ineedhousing.cronjob.keymaster.models;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import java.time.LocalDateTime;
 
+@RegisterForReflection
 public record ServiceVerificationDto(String apiToken, String serviceName, LocalDateTime timeStamp) {
 }
--- cron_job_service/src/main/java/ineedhousing/cronjob/keymaster/models/VerifiedServiceDto.java
+++ cron_job_service/src/main/java/ineedhousing/cronjob/keymaster/models/VerifiedServiceDto.java
@@ -1,6 +1,9 @@
 package ineedhousing.cronjob.keymaster.models;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import java.time.LocalDateTime;
 
+@RegisterForReflection
 public record VerifiedServiceDto(String authorizedStatus, LocalDateTime timeStamp) {
 }
