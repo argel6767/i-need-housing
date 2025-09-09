02a1c7bd9048a14b9fd03d18d56f3c577c9898ce
argel6767 <argel6767@gmail.com>
2025-09-08 22:05:12 -0400

    update pipline for email service

---
+++ scripts/push_email_image.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+import platform
+from build_backend_image import check_if_docker_is_running
+from push_backend_image import build_and_push_with_unique_tag, is_cicd_pipeline, load_env_file
+from push_new_listings_image import decode_gcp_service_account_key, create_tmp_service_acc_key_file, sign_in_to_gcp, \
+    configure_docker_for_gcr, make_gcp_image, deploy_new_image
+
+email_service = Path.cwd() / "email_service"
+isOsWindows = platform.system() == "Windows"
+app_service_name = "email-service"
+repo_name = "email-service-repo"
+
+def main():
+    project_id = os.environ["PROJECT_ID"]
+    check_if_docker_is_running()
+
+    if not is_cicd_pipeline():
+        load_env_file()
+
+    key = decode_gcp_service_account_key()
+    file_path = create_tmp_service_acc_key_file(key)
+    sign_in_to_gcp(file_path, project_id)
+    configure_docker_for_gcr()
+    image_name = build_and_push_with_unique_tag(repo_name, app_service_name, email_service, make_gcp_image)
+
+if __name__ == "__main__":
+    main()
--- scripts/push_keymaster_image.py
+++ scripts/push_keymaster_image.py
@@ -1,9 +1,6 @@
 import os
 from pathlib import Path
 import platform
-import subprocess
-import tempfile
-import base64
 from build_backend_image import check_if_docker_is_running
 from push_backend_image import build_and_push_with_unique_tag, is_cicd_pipeline, load_env_file
 from push_new_listings_image import decode_gcp_service_account_key, create_tmp_service_acc_key_file, sign_in_to_gcp, configure_docker_for_gcr, make_gcp_image, deploy_new_image
@@ -11,6 +8,7 @@ from push_new_listings_image import decode_gcp_service_account_key, create_tmp_s
 keymaster_service = Path.cwd()/"keymaster_service"
 isOSWindows = platform.system() == "Windows"
 app_service_name = "keymaster-service"
+repo_name = "keymaster-service-repo"
 
 def main():
     project_id = os.environ["PROJECT_ID"]
@@ -23,9 +21,8 @@ def main():
     file_path = create_tmp_service_acc_key_file(key=key)
     sign_in_to_gcp(file_path=file_path, project_id=project_id)
     configure_docker_for_gcr()
-    image_name = build_and_push_with_unique_tag(repo_name="keymaster-service-repo", service=app_service_name, directory=keymaster_service, image_name_creator=make_gcp_image)
+    image_name = build_and_push_with_unique_tag(repo_name=repo_name, service=app_service_name, directory=keymaster_service, image_name_creator=make_gcp_image)
     deploy_new_image(image_name=image_name, service_name=app_service_name)
-
 
 if __name__ == "__main__":
     main()
