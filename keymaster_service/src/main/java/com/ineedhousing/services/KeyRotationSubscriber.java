72895874626e005610a03208eea18c9ffef87418
Argel Hernandez <argel6767@gmail.com>
2025-09-09 19:33:39 -0400

    add missing RestClient annotation

--- keymaster_service/src/main/java/com/ineedhousing/services/KeyRotationSubscriber.java
+++ keymaster_service/src/main/java/com/ineedhousing/services/KeyRotationSubscriber.java
@@ -22,6 +22,7 @@ public class KeyRotationSubscriber {
     MainAPIEmailServiceRestClient mainAPIEmailServiceRestClient;
 
     @Inject
+    @RestClient
     EmailServiceRestClient emailServiceRestClient;
 
     @Inject
