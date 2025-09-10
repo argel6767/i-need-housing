3db32d9059eb66ae87a970bb8a31b03660fe0b5d
argel6767 <argel6767@gmail.com>
2025-09-09 21:22:21 -0400

    remove duplicate definitons

--- backend/src/test/java/com/ineedhousing/backend/configs/GoogleCronJobServiceNewListingsRetrievalRestClientConfigurationTest.java
+++ backend/src/test/java/com/ineedhousing/backend/configs/GoogleCronJobServiceNewListingsRetrievalRestClientConfigurationTest.java
@@ -6,8 +6,6 @@ import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.web.client.RestClient;
 
-import com.ineedhousing.backend.apis.NewListingsRetrievalRestClientConfiguration;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class GoogleCronJobServiceNewListingsRetrievalRestClientConfigurationTest {
