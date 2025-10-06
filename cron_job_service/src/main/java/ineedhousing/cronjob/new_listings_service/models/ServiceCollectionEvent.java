b7594e15204d98510f289908adaf01677e7de97c
argel6767 <argel6767@gmail.com>
2025-10-05 22:57:33 -0400

    register for reflection

--- cron_job_service/src/main/java/ineedhousing/cronjob/new_listings_service/models/ServiceCollectionEvent.java
+++ cron_job_service/src/main/java/ineedhousing/cronjob/new_listings_service/models/ServiceCollectionEvent.java
@@ -1,7 +1,10 @@
 package ineedhousing.cronjob.new_listings_service.models;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@RegisterForReflection
 public record ServiceCollectionEvent(ThirdPartyServiceName serviceName, String message, LocalDateTime timestamp, UUID jobId) {
 }
